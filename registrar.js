be1c22d8e5b8459cc14e49cbc3c94ba18c7c2ca6
LedSouzaSenac <ledsouza@senacrs.com.br>
2026-03-30 17:01:37 -0300

    feat: registra usuario em um array

--- registrar.js
+++ registrar.js
@@ -1,3 +1,10 @@
+const usuarios = []
+
 function registrar (email, senha) {
     console.log(`Olá! Você se registrou com o email ${email}`)
+    const usuario = {
+        email: email,
+        senha: senha
+    }
+    usuarios.push(usuario)
 }
